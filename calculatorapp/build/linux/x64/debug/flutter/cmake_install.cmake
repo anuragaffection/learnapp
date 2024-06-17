51239f94899cecf0d16b1ec3946652bef771840a
Anurag Affection <anuragaffection07@gmail.com>
2024-06-17 17:17:43 +0530

    updated

--- calculatorapp/build/linux/x64/debug/flutter/cmake_install.cmake
+++ calculatorapp/build/linux/x64/debug/flutter/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/zerokelvin/codework/flutterApps/learnapp/linux/flutter
+# Install script for directory: /home/zerokelvin/codework/flutterApps/learnapp/calculatorapp/linux/flutter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/zerokelvin/codework/flutterApps/learnapp/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/zerokelvin/codework/flutterApps/learnapp/calculatorapp/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
